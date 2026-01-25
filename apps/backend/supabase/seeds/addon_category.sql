5f799d140fe806877f76b9ea35056bd8cb7561bc
lonewanderer27 <adrianejames27@gmail.com>
2026-01-26 02:24:37 +0800

    feat(seed): addon category

---
+++ apps/backend/supabase/seeds/addon_category.sql
@@ -0,0 +1,4 @@
+insert into addon_category (name, description)
+values 
+    ('milk', 'The drink balances coffee strength and milk softness, typically served in small cups of 120–150 ml.'),
+    ('syrup', 'Thick, viscous, liquid condiment consisting primarily of a solution of sugar in water. ');
