055c6afba78542197833d6c1a2aef39071ed0458
lonewanderer27 <adrianejames27@gmail.com>
2026-01-27 22:43:52 +0800

    feat(app-theme): set default mode to iOS

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -45,7 +45,9 @@ import '@ionic/react/css/palettes/dark.system.css';
 /* Theme variables */
 import './theme/variables.css';
 
-setupIonicReact();
+setupIonicReact({
+  mode: 'ios'
+});
 
 const App: React.FC = () => (
   <IonApp>
