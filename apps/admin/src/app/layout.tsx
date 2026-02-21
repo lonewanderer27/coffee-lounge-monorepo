e86447e428d9e05b2aee9d15467dcc69f324a446
lonewanderer27 <adrianejames27@gmail.com>
2026-02-21 17:18:21 +0800

    chore(admin): remove github banner

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -34,7 +34,6 @@ export default async function RootLayout({
     <html lang="en">
       <body>
         <Suspense>
-          <GitHubBanner />
           <RefineKbarProvider>
             <AntdRegistry>
               <ColorModeContextProvider defaultMode={defaultMode}>
